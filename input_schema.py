a1cb99b87e07c064e4338f5d88b869e109d50e15
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-09-21 22:28:20 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "image_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://raw.githubusercontent.com/open-mmlab/mmdeploy/main/tests/data/tiger.jpeg"]
+    }
+}
